9b894f98eaf96fc751c5905edf656ce459a34135
jiju <jijukjose@gmail.com>
2015-09-15 20:41:28 +0200

    more directives

---
+++ app/js/directives/album-table-directive.js
@@ -0,0 +1,10 @@
+tidalApp.directive('albumTable', function () {
+
+    return {
+        restrict: 'E',
+        templateUrl: '/js/directives/template/album-table.html',
+        controller: function () {
+        }
+    };
+
+});
